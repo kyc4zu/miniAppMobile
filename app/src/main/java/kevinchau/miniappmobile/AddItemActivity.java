2f91deb57c1da82330da609cf6682d86dab18153
Kevin Chau <kyc4zu@virginia.edu>
2017-01-29 21:28:21 -0500

    Desktop Additions

--- app/src/main/java/kevinchau/miniappmobile/AddItemActivity.java
+++ app/src/main/java/kevinchau/miniappmobile/AddItemActivity.java
@@ -3,16 +3,49 @@ package kevinchau.miniappmobile;
 import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.EditText;
+
 public class AddItemActivity extends AppCompatActivity{
+    EditText nameField;
+    EditText descField;
+    EditText latNum;
+    EditText longNum;
+    CalendarView calendar;
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        descField = (EditText)findViewById(R.id.descriptionText);
+        nameField = (EditText)findViewById(R.id.nameText);
+        latNum = (EditText)findViewById(R.id.latText);
+        longNum = (EditText)findViewById(R.id.longText);
+        calendar = (CalendarView)findViewById(R.id.calendarView);
         super.onCreate(savedInstanceState);
         setTitle("Add New Item");
         setContentView(R.layout.itemactivity_main);
 
     }
+    // Called when you tap the Save New Item Button
+    public void saveNewItem(EditText name, EditText desc, EditText lat, EditText lon, CalendarView cal) {
+        // Make sure it is a name
+        if(nameField.getText().toString() != null && !nameField.getText().toString().equals("")) {
+            // Log the action
+            Log.d("ListExample", "addContact " + nameField.getText().toString());
+            // Make a new contact
+            contacts.add(new Contact(nameField.getText().toString(), true));
+            // Get the adapter that manages the data set and let it know something new was added
+            rvContacts.getAdapter().notifyDataSetChanged();
+            // Blank the name field
+            nameField.setText("");
+        }
+    }
+
 }
---
+++ app/src/main/java/kevinchau/miniappmobile/BucketItem.java
@@ -0,0 +1,79 @@
+package kevinchau.miniappmobile;
+
+/**
+ * Created by Brittany Crow on 1/28/2017.
+ */
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class BucketItem {
+
+    private String mName;
+    private String mDescription;
+    private double mLatitude;
+    private double mLongitude;
+    private int mDate;
+
+    public BucketItem(String name, String description, double latitude, double longitude, int date) {
+
+        mName = name;
+        mDescription = description;
+        mLatitude = latitude;
+        mLongitude = longitude;
+        mDate = date;
+
+    }
+
+    public String getName() {
+
+        return mName;
+
+    }
+
+    public String getDesc() {
+
+        return mDescription;
+
+    }
+
+    public double getLat() {
+
+        return mLatitude;
+
+    }
+
+    public double getLong() {
+
+        return mLongitude;
+
+    }
+
+    public int getDate() {
+
+        return mDate;
+
+    }
+
+    private static int lastItemId = 0;
+
+    public static ArrayList<BucketItem> createBucketList(int numItems) {
+
+        ArrayList<BucketItem> bucketlist = new ArrayList<BucketItem>();
+
+        Calendar c = Calendar.getInstance();
+        int date = c.get(Calendar.DATE);
+
+        /*for (int i = 1; i <= numItems; i++) {
+            bucketlist.add(new BucketItem("Item #" + ++lastItemId,"", 0.0, 0.0, date));
+        }*/
+
+        bucketlist.add(new BucketItem("Get the first Bodos ticket","Be the first person to order at Bodos on the Corner.", 0.0, 0.0, date));
+        bucketlist.add(new BucketItem("Apply for graduation","Make sure all major and minor requirements are fulfilled.", 0.0, 0.0, date));
+        bucketlist.add(new BucketItem("Visit Monticello","Visit the home of our founder.", 0.0, 0.0, date));
+
+        return bucketlist;
+
+    }
+
+}
